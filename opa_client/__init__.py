d8e29ed50356691253f0201c4fced5f8071fb745
turall <tural_m@hotmail.com>
2024-12-11 10:17:02 +0400

    update version

--- opa_client/__init__.py
+++ opa_client/__init__.py
@@ -11,7 +11,7 @@ def create_opa_client(async_mode=False, *args, **kwargs):
 		return OpaClient(*args, **kwargs)
 
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Tural Muradov"
 __license__ = "MIT"
 
